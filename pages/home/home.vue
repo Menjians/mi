ccadd0c949058ada2b6ab4038804d00d24b1c843
忆梦璃 <1132754466@qq.com>
2021-05-05 16:31:58 +0800

    git add .!

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,143 +1,133 @@
 <template>
-	<view>
-		<view class="header">
-			<view class="logo">
-				<image src="../../static/img/logo.png" mode=""></image>
-			</view>
-			<view class="search">
-				<image src="../../static/img/search.png" ></image>
-				<text>搜索商品名称</text>
-			</view>
-			<view class="user">
-				<image src="../../static/icon/user.png" mode=""></image>
-			</view>
+  <view>
+    <view class="header">
+      <view class="logo">
+        <image src="../../static/img/logo.png" mode=""></image>
+      </view>
+      <view class="search">
+        <image src="../../static/img/search.png"></image>
+        <text>搜索商品名称</text>
+      </view>
+      <view class="user">
+        <image src="../../static/icon/user.png" mode=""></image>
+      </view>
+    </view>
+    <view class="nav">
+      <ul>
+        <li>
+          <text :class="{ active: navIndex === -1 }" @click="changeNav(-1)"
+            >推荐</text
+          >
+        </li>
+        <li
+          v-for="(item, index) in navItems"
+          :key="index"
+          @click="changeNav(index)"
+        >
+          <text :class="{ active: navIndex === index }">{{ item }}</text>
+        </li>
+      </ul>
 		</view>
-		<view class="nav">
-			<ul>
-				<li><text :class="{active:navIndex===-1}" @click="changeNav(-1)">推荐</text></li>
-				<li v-for="(item,index) in navItems"  :key="index" @click="changeNav(index)">
-					<text :class="{active:navIndex===index}">{{item}}</text>
-				</li>
-			</ul>
-		</view>
-		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" 
-		indicator-active-color="#fff" indicator-color="#ccc">
-			<swiper-item>
-				<view class="swiper-item">
-					<image src="../../static/img/xiaomiUltra.jpg"></image>
-				</view>
-			</swiper-item>
-			<swiper-item>
-				<view class="swiper-item">
-					<image src="../../static/img/mifold.jpg" mode=""></image>
-				</view>
-			</swiper-item>
-		</swiper>
-	</view>
+    <Recommend></Recommend>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				navItems:["手机","智能","电视","家电","笔记本"],
-				navIndex:-1
-			}
-		},
-		methods: {
-			changeNav(index){
-				this.navIndex = index
-			}
-		}
-	}
+import Recommend from "../recommend/recommend.vue";
+export default {
+  components: {
+    Recommend,
+  },
+  data() {
+    return {
+      navItems: ["手机", "智能", "电视", "家电", "笔记本"],
+      navIndex: -1,
+    };
+  },
+  methods: {
+    changeNav(index) {
+      this.navIndex = index;
+    },
+  },
+};
 </script>
 
 <style lang="less">
-	* {
-		margin: 0;
-		padding: 0;
-	}
-	swiper{
-		width: 100%;
-		height: 187.5*2rpx;
-	}
-	.swiper-item{
-		image{
-			width: 100%;
-			height: 187.5*2rpx;
-		}
-	}
-	.nav{
-		width: 100%;
-		background-color: #f2f2f2;
-		ul{
-			display: flex;
-			width: 100%;
-			height: 60rpx;
-			li{
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				flex-grow: 1;
-				list-style: none;
-				text{
-					color: #747474;
-					box-sizing: border-box;
-					line-height: 60rpx;
-					font-size: 28rpx;
-					height: 60rpx;
-				}
-				.active{
-					color: #ff6700;
-					border-bottom: 2px solid #ff6700;
-				}
-			}
-		}
-	}
-	.header {
-		width: 100%;
-		height: 43.76*2rpx;
-		display: flex;
-		align-items: center;
-		background-color: #f2f2f2;
 
-		.logo {
-			display: flex;
-			height: 100%;
-			width: 52.09*2rpx;
-			justify-content: center;
-			align-items: center;
+* {
+  margin: 0;
+  padding: 0;
+}
+.nav {
+  width: 100%;
+  background-color: #f2f2f2;
+  ul {
+    display: flex;
+    width: 100%;
+    height: 60rpx;
+    li {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-grow: 1;
+      list-style: none;
+      text {
+        color: #747474;
+        box-sizing: border-box;
+        line-height: 60rpx;
+        font-size: 28rpx;
+        height: 60rpx;
+      }
+      .active {
+        color: #ff6700;
+        border-bottom: 2px solid #ff6700;
+      }
+    }
+  }
+}
+.header {
+  width: 100%;
+  height: 43.76 * 2rpx;
+  display: flex;
+  align-items: center;
+  background-color: #f2f2f2;
 
-			image {
-				width: 25*2rpx;
-				height: 16.44*2rpx;
-			}
-		}
-		.user {
-			width: 52.09*2rpx;
-			height: 31.26*2rpx;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			image {
-				width: 20*2rpx;
-				height: 20*2rpx;
-			}
-		}
-		.search{
-			border-radius: 4rpx;
-			flex-grow: 1;
-			height: 33.26*2rpx;
-			background-color: #FFFFFF;
-			display: flex;
-			color: #b2b2b2;
-			align-items: center;
-			image{
-				margin: 0 10rpx;
-				width: 20*2rpx;
-				height: 20*2rpx;
-			}
-		}
+  .logo {
+    display: flex;
+    height: 100%;
+    width: 52.09 * 2rpx;
+    justify-content: center;
+    align-items: center;
 
-	}
+    image {
+      width: 25 * 2rpx;
+      height: 16.44 * 2rpx;
+    }
+  }
+  .user {
+    width: 52.09 * 2rpx;
+    height: 31.26 * 2rpx;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    image {
+      width: 20 * 2rpx;
+      height: 20 * 2rpx;
+    }
+  }
+  .search {
+    border-radius: 4rpx;
+    flex-grow: 1;
+    height: 33.26 * 2rpx;
+    background-color: #ffffff;
+    display: flex;
+    color: #b2b2b2;
+    align-items: center;
+    image {
+      margin: 0 10rpx;
+      width: 20 * 2rpx;
+      height: 20 * 2rpx;
+    }
+  }
+}
 </style>
---
+++ pages/recommend/recommend.vue
@@ -0,0 +1,111 @@
+<template>
+	<view>
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000"
+		indicator-active-color="#fff" indicator-color="#ccc">
+			<swiper-item>
+				<view class="swiper-item">
+					<image src="../../static/img/xiaomiUltra.jpg"></image>
+				</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">
+					<image src="../../static/img/mifold.jpg" mode=""></image>
+				</view>
+			</swiper-item>
+		</swiper>
+		<ul class="navList">
+			<li><image src="../../static/imgnav/xmms.png" mode=""></image></li>
+			<li><image src="../../static/imgnav/xmzc.webp.jpg"></li>
+			<li><image src="../../static/imgnav/Note9.webp.jpg"></li>
+			<li><image src="../../static/imgnav/yjhx.webp.jpg"></li>
+			<li><image src="../../static/imgnav/xmsx.webp.jpg"></li>
+			<li><image src="../../static/imgnav/zn.webp.jpg"></li>
+			<li><image src="../../static/imgnav/bjbrm.png"></li>
+			<li><image src="../../static/imgnav/dsrm.png"></li>
+			<li><image src="../../static/imgnav/xyjrm.webp.jpg"></li>
+			<li><image src="../../static/imgnav/mfk.png"></li>
+		</ul>
+		<view class="divider"></view>
+		<view class="container">
+			<view class="viewleft">
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/29b39579739d82269d18f0e7c8f6c9da.jpg?f=webp&w=537&h=762&bg=57524C"></image>
+			</view>
+			<view class="viewright">
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5b9af94e5c05bb2b4849cbb113eddb9d.jpg?f=webp&w=537&h=378&bg=EBF8FF"></image>
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/42e02f30c05282812529e499211f4c85.png?f=webp&w=537&h=378&bg=E9F7FF"></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	*{
+		margin: 0;
+		padding: 0;
+	}
+	.container{
+		width: 100%;
+		height: 264.71*2rpx;
+		display: flex;
+		justify-content: space-between;
+		.viewleft,.viewright{
+			width: 186.55*2rpx;
+			height: 264.71*2rpx;
+		}
+		.viewleft{
+			image{
+				width: 186.55*2rpx;
+				height: 264.71*2rpx;
+			}
+		}
+		.viewright{
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			image{
+				width: 186.55*2rpx;
+				height: 131.31*2rpx;
+			}
+		}
+	}
+	.divider{
+		width: 100%;
+		height: 8.34 * 2rpx;
+		background: #f5f5f5;
+	}
+	.navList{
+		display: flex;
+		flex-wrap: wrap;
+		li{
+			list-style: none;
+			flex: 20%;
+			image{
+				width: 75.04*2rpx;
+				height: 79.02*2rpx;
+			}
+		}
+	}
+swiper{
+		width: 100%;
+		height: 187.5*2rpx;
+	}
+	.swiper-item{
+		image{
+			width: 100%;
+			height: 187.5*2rpx;
+		}
+	}
+</style>
