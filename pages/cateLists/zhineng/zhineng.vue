bb19b582ba500c6319512b3f5bbcd751426b08ff
忆梦璃 <1132754466@qq.com>
2021-05-11 16:26:34 +0800

    完成智能模块

--- pages/cateLists/zhineng/zhineng.vue
+++ pages/cateLists/zhineng/zhineng.vue
@@ -43,6 +43,75 @@
 			<image src="https://i8.mifile.cn/v1/a1/72411ddf-3df9-8d3a-7e5d-9429991710dc.webp" mode=""></image>
 		</view>
 		<view class="line"></view>
+		<view class="hotListouter">
+		
+		<ul class="hotList">
+			<li>
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/4e8374f7c5071c5e4f81adb02a62f8f9.jpg?thumb=1&w=221&h=221" mode=""></image>
+				<view class="outer">
+					<view class="name">扫拖一体机器人</view>
+					<view class="brief">扫得干净，拖得彻底</view>
+					<view class="price"><text>¥1399</text><text class="del">¥1999</text></view>
+				</view>
+			</li><li>
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/62927fecf01e010e55a22158b967e447.jpg?thumb=1&w=221&h=221" mode=""></image>
+				<view class="outer">
+					<view class="name">小米手环5</view>
+					<view class="brief">11种运动模式 磁吸式充电</view>
+					<view class="price"><text>¥169</text><text class="del">¥189</text></view>
+				</view>
+			</li><li>
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/26c076c82d449b3898b9d3df3209c5c8.jpg?thumb=1&w=221&h=221" mode=""></image>
+				<view class="outer">
+					<view class="name">净水器600G</view>
+					<view class="brief">600加仑通量，流速更快</view>
+					<view class="price"><text>¥1499</text><text class="del">¥1699</text></view>
+				</view>
+			</li><li>
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0d5e99210892f75ed0e0c1f00a1872dc.jpg?thumb=1&w=221&h=221" mode=""></image>
+				<view class="outer">
+					<view class="name">Redmi AirDots 2真无线蓝牙耳机</view>
+					<view class="brief">支持蓝牙5.0，自动秒连，拿起就能用</view>
+					<view class="price"><text>¥99</text></view>
+				</view>
+			</li><li>
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/ac481fb38b20256fd2b9c82f00044d86.jpg?thumb=1&w=221&h=221" mode=""></image>
+				<view class="outer">
+					<view class="name">米家无线除螨仪</view>
+					<view class="brief">每个家庭都需要一台除螨仪</view>
+					<view class="price"><text>¥529</text><text class="del">¥549</text></view>
+				</view>
+			</li><li>
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/01540184bd88d30e910347f70357142d.jpg?thumb=1&w=221&h=221" mode=""></image>
+				<view class="outer">
+					<view class="name">Redmi小爱触屏音箱 8英寸</view>
+					<view class="brief">超大屏，让智能更多可能</view>
+					<view class="price"><text>¥349</text></view>
+				</view>
+			</li><li>
+				<image src="https://i8.mifile.cn/v1/a1/6f2954f1-9b60-6d87-ffc1-ca203190fdb7.webp" mode=""></image>
+				<view class="outer">
+					<view class="name">小米AI音箱</view>
+					<view class="brief">听音乐、语音遥控家电</view>
+					<view class="price"><text>¥229</text></view>
+				</view>
+			</li><li>
+				<image src="https://i8.mifile.cn/v1/a1/552b0df8-6a5c-f4dc-5a2e-12ed62a86bdf.webp" mode=""></image>
+				<view class="outer">
+					<view class="name">小米体脂秤2</view>
+					<view class="brief">轻松掌握身体脂肪率</view>
+					<view class="price"><text>¥99</text></view>
+				</view>
+			</li><li>
+				<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/dfb3853e83ef0e862aab7c3537a940b0.jpg?thumb=1&w=221&h=221" mode=""></image>
+				<view class="outer">
+					<view class="name">小米智能门锁 Pro</view>
+					<view class="brief">旗舰摄像头门锁，安全一步到位</view>
+					<view class="price"><text>¥1599</text><text class="del">¥1699</text></view>
+				</view>
+			</li>
+		</ul>
+		</view>
 	</view>
 </template>
 
@@ -64,6 +133,58 @@
 		margin: 0;
 		padding: 0;
 	}
+	.hotListouter{
+		width: 100%;
+		.hotList{
+			display: flex;
+			width: 100%;flex-wrap: wrap;
+			li{
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				flex: 32.33%;
+				list-style: none;
+				image{
+					width: 115*2rpx;
+					height: 115*2rpx;
+				}
+				.outer{
+					height: 75.42*2rpx;
+					width: 115rpx * 2;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					flex-direction: column;
+					.name{
+						font-size: 26rpx;
+						font-weight: bold;
+					}
+					.brief{
+						font-size: 24rpx;
+					}
+					.name,.brief{
+						white-space: nowrap;
+						text-overflow: ellipsis;
+						overflow: hidden;
+						width: 104*2rpx;
+						text-align: center;
+					}
+					.price{
+						color: #F54B4B;
+						font-size: 24rpx;
+						font-weight: bold;
+						.del{
+							color: #000;
+							text-decoration: line-through;
+							margin-left: 10rpx;
+						}
+					}
+				}
+			}
+		}
+	}
+	
 	.rmbp{
 		image{
 			height: 58.36 * 2rpx;
