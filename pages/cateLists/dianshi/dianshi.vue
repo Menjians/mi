6417b02cc123019b0d4f495bee57e3199ccaae51
忆梦璃 <1132754466@qq.com>
2021-05-13 11:23:10 +0800

    完成电视功能开发

--- pages/cateLists/dianshi/dianshi.vue
+++ pages/cateLists/dianshi/dianshi.vue
@@ -1,6 +1,135 @@
 <template>
-	<view>
-		电视
+	<view class="dscontainer">
+		<view class="hotrecommend">
+			<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c0bcf1381f9a43f60b39b58e891f6daa.jpg?f=webp" mode=""></image>
+		</view>
+		<view class="line"></view>
+		
+		<view class="big">
+			<view>
+				<view class="bigimg">
+					<view class="pos">
+						<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c691b8a8dc7e3a37bdea7bd5eb6eaf3c.jpg?thumb=1&w=338&h=338" mode=""></image>
+						<image class="mx" src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/57817f3ee2b04f655c5d98c255ed9a9a.png?w=167&h=167" mode=""></image>
+					</view>
+				</view>
+				<view class="info">
+					<text class="name">全面屏电视  E55X</text>
+					<text class="text">潮流全面屏设计，4K超高清画质</text>
+					<view>
+						<text class="price">¥2388</text> <text class="old-price">¥2599</text>
+					</view>
+					<view class="btn">立即购买</view>
+				</view>
+			</view>
+			<view>
+				<view class="bigimg">
+					<view class="pos">
+						<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/fe3f37e6f1ad601e1d8348195d620662.jpg?thumb=1&w=338&h=338" mode=""></image>
+						<image class="mx" src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/57817f3ee2b04f655c5d98c255ed9a9a.png?w=167&h=167" mode=""></image>
+					</view>
+				</view>
+				<view class="info">
+					<text class="name">小米全面屏电视 E65X</text>
+					<text class="text">全面屏设计搭配高质量画质</text>
+					<view>
+						<text class="price">¥3088</text> <text class="old-price">¥3299</text>
+					</view>
+					<view class="btn">立即购买</view>
+				</view>
+			</view>
+			
+		</view>
+		<view class="Tvlist">
+			<view>
+				<view class="smallimg">
+					<view class="pos">
+						<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/20d970b239e6b7152d07b6282e160192.jpg?thumb=1&w=221&h=221" mode=""></image>
+						<image class="mx" src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/57817f3ee2b04f655c5d98c255ed9a9a.png?w=167&h=167" mode=""></image>
+					</view>
+				</view>
+				<view class="info">
+					<text class="name">全面屏E32C</text>
+					<text class="text">全面屏设计，人工智能系统</text>
+					<view>
+						<text class="price">¥949</text> <text class="old-price">¥1099</text>
+					</view>
+				</view>
+			</view>
+			<view>
+				<view class="smallimg">
+					<view class="pos">
+						<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c1bb566a499a4691cf878bb3ac23c056.jpg?thumb=1&w=221&h=221" mode=""></image>
+						<image class="mx" src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/57817f3ee2b04f655c5d98c255ed9a9a.png?w=167&h=167" mode=""></image>
+					</view>
+				</view>
+				<view class="info">
+					<text class="name">小米电视4A 70英寸</text>
+					<text class="text">大屏更享受</text>
+					<view>
+						<text class="price">¥3688</text> <text class="old-price">¥3999</text>
+					</view>
+				</view>
+			</view>
+			<view>
+				<view class="smallimg">
+					<view class="pos">
+						<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/abf7219dc5dd88a5a425e05d66459c1e.jpg?thumb=1&w=221&h=221" mode=""></image>
+						<image class="mx" src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/57817f3ee2b04f655c5d98c255ed9a9a.png?w=167&h=167" mode=""></image>
+					</view>
+				</view>
+				<view class="info">
+					<text class="name">小米电视4A 60英寸</text>
+					<text class="text">人工智能语音系统,超高清画质</text>
+					<view>
+						<text class="price">¥2788</text> <text class="old-price">¥2799</text>
+					</view>
+				</view>
+			</view>
+		</view><view class="Tvlist">
+			<view>
+				<view class="smallimg">
+					<view class="pos">
+						<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/26eb3bb463c62c0be77dfd04cae7a703.jpg?thumb=1&w=221&h=221" mode=""></image>
+					</view>
+				</view>
+				<view class="info">
+					<text class="name">全面屏Pro43"	</text>
+					<text class="text">全金属机身，2G+32G大存储，支持8K视频解码</text>
+					<view>
+						<text class="price">¥1799</text> <text class="old-price">¥1999</text>
+					</view>
+				</view>
+			</view>
+			<view>
+				<view class="smallimg">
+					<view class="pos">
+						<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/37c64856e3d7a35557049bb5e5c9e687.jpg?thumb=1&w=221&h=221" mode=""></image>
+					</view>
+				</view>
+				<view class="info">
+					<text class="name">Redmi电视A55</text>
+					<text class="text">澎湃音效影院级体验</text>
+					<view>
+						<text class="price">¥2188</text> <text class="old-price">¥2199</text>
+					</view>
+				</view>
+			</view>
+			<view>
+				<view class="smallimg">
+					<view class="pos">
+						<image src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/162b79e71038fc5423b5e7d8880d7b4a.jpg?thumb=1&w=221&h=221" mode=""></image>
+					</view>
+				</view>
+				<view class="info">
+					<text class="name">全面屏E43K</text>
+					<text class="text">全面屏设计，人工智能系统</text>
+					<view>
+						<text class="price">¥1388</text> <text class="old-price">¥1599</text>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -17,6 +146,159 @@
 	}
 </script>
 
-<style>
-
+<style lang="less" scoped>
+*{
+	margin: 0;
+	padding: 0;
+}
+.dscontainer{
+	background-color: #FFB29D;
+	padding: 0 16rpx;
+}
+.Tvlist{
+	display: flex;
+	width: 100%;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	margin-bottom: 20rpx;
+	.smallimg{
+		width: 115.09 * 2rpx;
+		height: 115.09 * 2rpx;
+		.pos{
+			image{
+				width: 115.09 * 2rpx;
+				height: 115.09 * 2rpx;
+			}
+			.mx{
+				position: absolute;
+				width: 52 * 2rpx;
+				height: 52*2rpx;
+				right: 2rpx;
+				bottom: 5rpx;
+			}
+		}
+	}
+	.info{
+		display: flex;
+		background-color: #fff;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 115 * 2rpx;
+		height: 75.42 * 2rpx;
+		.price{
+			color: #f54b4b;
+			font-size: 17*2rpx;
+			font-weight: bold;
+		}
+		.old-price{
+			color: #464646;
+			text-decoration: line-through;
+			margin-left: 10rpx;
+			font-size: 22rpx;
+			font-weight: bold;
+		}
+		.name,.text{
+			width: 104.63 * 2rpx;
+			text-align: center;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			color: #3c3c3c;
+		}
+		.text{
+			font-size: 12.61*2rpx;
+		}
+		.name{
+			font-weight: bold;
+			font-size: 28rpx;
+		}
+	}
+	
+}
+.big{
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 15rpx;
+	.bigimg{
+		.pos{
+			position: relative;
+			width: 176*2rpx;
+			height: 176*2rpx;
+			image{
+				width: 176*2rpx;
+				height: 176*2rpx;
+				vertical-align: bottom;
+			}
+			.mx{
+				position: absolute;
+				width: 52 * 2rpx;
+				height: 52*2rpx;
+				right: 2rpx;
+				bottom: 5rpx;
+			}
+		}
+	}
+	.info{
+		display: flex;
+		background-color: #fff;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 176 * 2rpx;
+		height: 113 * 2rpx;
+		.price{
+			color: #f54b4b;
+			font-size: 17*2rpx;
+			font-weight: bold;
+		}
+		.old-price{
+			color: #464646;
+			text-decoration: line-through;
+			margin-left: 10rpx;
+			font-size: 22rpx;
+			font-weight: bold;
+		}
+		.btn{
+			width: 91.66*2rpx;
+			background-color: #f54b4b;
+			height: 25.52 * 2rpx;
+			color: white;
+			font-size: 26rpx;
+			text-align: center;
+			border-radius: 8rpx;
+			line-height: 25.2 * 2rpx;
+			
+		}
+		.name,.text{
+			width: 165.63 * 2rpx;
+			text-align: center;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			color: #3c3c3c;
+		}
+		.text{
+			font-size: 12.61*2rpx;
+		}
+		.name{
+			font-weight: bold;
+			font-size: 28rpx;
+		}
+	}
+}
+.line{
+	height: 8.33*2rpx;
+	width: 100%;
+}
+// page{
+// 	background-color: #FFB29D;
+// 	padding: 0 16rpx;
+// }
+.hotrecommend{
+	image{
+		width: 100%;
+		height: 62.5 * 2rpx;
+	}
+}
 </style>
